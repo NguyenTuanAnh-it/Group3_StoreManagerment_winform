bede53cf15aceff54f0d5a2ee81d2782545087a5
Nguyễn Tuấn Anh <anhnguyen2003contact@gmail.com>
2024-11-13 19:21:41 +0700

    Fix ket noi, form dang nhap ket noi database

---
+++ BLT_Nhom3/BLT_Nhom3/DangNhap.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BLT_Nhom3
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txb_user = new System.Windows.Forms.TextBox();
+            this.txb_pass = new System.Windows.Forms.TextBox();
+            this.cb_quanLy = new System.Windows.Forms.CheckBox();
+            this.btn_dangNhap = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng Nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tài khoản:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật khẩu:";
+            // 
+            // txb_user
+            // 
+            this.txb_user.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_user.Location = new System.Drawing.Point(174, 67);
+            this.txb_user.Name = "txb_user";
+            this.txb_user.Size = new System.Drawing.Size(184, 21);
+            this.txb_user.TabIndex = 3;
+            // 
+            // txb_pass
+            // 
+            this.txb_pass.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_pass.Location = new System.Drawing.Point(174, 124);
+            this.txb_pass.Name = "txb_pass";
+            this.txb_pass.PasswordChar = '*';
+            this.txb_pass.Size = new System.Drawing.Size(184, 21);
+            this.txb_pass.TabIndex = 4;
+            // 
+            // cb_quanLy
+            // 
+            this.cb_quanLy.AutoSize = true;
+            this.cb_quanLy.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_quanLy.Location = new System.Drawing.Point(289, 151);
+            this.cb_quanLy.Name = "cb_quanLy";
+            this.cb_quanLy.Size = new System.Drawing.Size(69, 17);
+            this.cb_quanLy.TabIndex = 5;
+            this.cb_quanLy.Text = "Quản lý";
+            this.cb_quanLy.UseVisualStyleBackColor = true;
+            // 
+            // btn_dangNhap
+            // 
+            this.btn_dangNhap.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dangNhap.Location = new System.Drawing.Point(157, 192);
+            this.btn_dangNhap.Name = "btn_dangNhap";
+            this.btn_dangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btn_dangNhap.TabIndex = 6;
+            this.btn_dangNhap.Text = "Đăng nhập";
+            this.btn_dangNhap.UseVisualStyleBackColor = true;
+            this.btn_dangNhap.Click += new System.EventHandler(this.btn_dangNhap_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.Location = new System.Drawing.Point(283, 192);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 7;
+            this.btn_exit.Text = "Thoát";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 285);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_dangNhap);
+            this.Controls.Add(this.cb_quanLy);
+            this.Controls.Add(this.txb_pass);
+            this.Controls.Add(this.txb_user);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.DangNhap_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txb_user;
+        private System.Windows.Forms.TextBox txb_pass;
+        private System.Windows.Forms.CheckBox cb_quanLy;
+        private System.Windows.Forms.Button btn_dangNhap;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
---
+++ BLT_Nhom3/BLT_Nhom3/DangNhap.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BLT_Nhom3
+{
+    public partial class DangNhap : Form
+    {
+        string chuoi = new SQL().getChuoi();
+        SqlConnection ketnoi;
+        SqlCommand thaotac;
+        SqlDataReader docdulieu;
+        public DangNhap()
+        {
+            InitializeComponent();
+            ketnoi = new SqlConnection(chuoi);
+        }
+
+        private void DangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_dangNhap_Click(object sender, EventArgs e)
+        {
+            if (txb_user.Text == "" || txb_pass.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập tài khoản", "Thông báo!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            else
+            {
+                string pQ = "";
+                ketnoi.Open();
+                String lenh = @"SELECT phanQuyen FROM DangNhap WHERE tenTK = '" + txb_user.Text + "' AND mKhau = '" + txb_pass.Text + "'";
+                thaotac = new SqlCommand(lenh, ketnoi);
+                docdulieu = thaotac.ExecuteReader();
+                if (!docdulieu.Read())
+                {
+                    MessageBox.Show("Tên đăng nhập hoặc mật khẩu sai!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+                else
+                {
+                    pQ = docdulieu[0].ToString();
+
+
+                }
+                ketnoi.Close();
+                docdulieu.Close();
+
+
+
+                if (pQ == "AD" && cb_quanLy.Checked)
+                {
+                    this.Hide();
+                    Form1 frm_main = new Form1();
+                    frm_main.ShowDialog();
+                    this.Show();
+                }
+                else if (pQ == "NV" && !cb_quanLy.Checked)
+                {
+                    this.Hide();
+                    Form1 frm_main = new Form1();
+                    frm_main.gioiHanNV();
+                    frm_main.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Không đủ quyển hạn!");
+                }
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
